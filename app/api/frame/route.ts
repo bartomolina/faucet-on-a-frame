dee1d0c014a089e3261d45af1e637c8d2f497555
Barto Molina <bartomolina@gmail.com>
2024-02-08 19:02:56 -0300

    updated

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -2,12 +2,22 @@ import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/o
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 import { ethers } from 'ethers';
+import { supabase } from '../../../lib/supabase-client';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
+  let accountAddress: string | undefined = '';
+  let fid: number | undefined;
   let destinationAddress: string | undefined = '';
 
   const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+  const { isValid, message } = await getFrameMessage(body, {
+    neynarApiKey: 'NEYNAR_ONCHAIN_KIT',
+  });
+
+  if (isValid) {
+    accountAddress = message.interactor.verified_accounts[0];
+    fid = message.interactor.fid;
+  }
 
   if (message?.input) {
     destinationAddress = message.input;
@@ -18,16 +28,23 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_URL);
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
 
-  const tx = await signer.sendTransaction({
-    to: '0x92d3267215Ec56542b985473E73C8417403B15ac',
-    value: ethers.parseUnits('0.1', 'ether'),
+  const { data, error } = await supabase.from('Users').upsert({
+    id: fid,
+    claimed_at: new Date().toISOString(),
   });
+
+  // const tx = await signer.sendTransaction({
+  //   to: '0x92d3267215Ec56542b985473E73C8417403B15ac',
+  //   value: ethers.parseUnits('0.1', 'ether'),
+  // });
+
+  // return new NextResponse('ok');
 
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `0.1 Sepolia ETH sent! ${tx.hash}`,
+          label: `0.1 Sepolia ETH sent!`,
         },
       ],
       image: `${NEXT_PUBLIC_URL}/start.png`,
